a55f3f9be6597c55a680eb5a5406a905e98c730a
Tali Herzka <tali@herzka.com>
2015-01-06 22:09:47 -0800

    created autoprotocol_core directory, updated readme

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name = 'autoprotocol-core',
     version = '1.0.0',
-    url='http://www.autoprotocol.org',
     packages = find_packages(),
     license = 'MIT',
     long_description = open('README.md').read(),
-    install_requires = ['autoprotocol-python==1.0.0'],
+    install_requires = ['autoprotocol==1.0.0'],
     test_suite="tests"
 )
